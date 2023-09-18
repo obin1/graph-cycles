3744663b292ed7ad36cfee8629de343b9a9d55e6
obin1 <psturm@usc.edu>
2023-09-18 10:47:41 -0700

    add a class to read in cases

---
+++ GEOS-Chem/ChemicalCase.py
@@ -0,0 +1,54 @@
+class ChemicalCase:
+    def __init__(self, filename):
+        self.filename = filename
+        self.parse_file()
+
+    def parse_file(self):
+        with open(self.filename, 'r') as f:
+            lines = f.readlines()
+            for i, line in enumerate(lines):
+                if line.startswith('Meteorological Fields'):
+                    self.parse_meteorological_fields(lines[i+1:])
+                elif line.startswith('Integrator-specific parameters'):
+                    self.parse_integrator_specific_parameters(lines[i+1:])
+                elif line.startswith('Chemical state'):
+                    self.parse_chemical_state(lines[i+1:])
+
+    def parse_meteorological_fields(self, lines):
+        self.timestamp = lines[0].split('Timestamp:')[1].strip()
+        self.longitude = float(lines[1].split(':')[1].strip())
+        self.latitude = float(lines[2].split(':')[1].strip())
+        self.level = int(lines[3].split(':')[1].strip())
+        self.temperature = float(lines[4].split(':')[1].strip())
+        self.pressure = float(lines[5].split(':')[1].strip())
+        self.air_number_density = float(lines[6].split(':')[1].strip())
+        self.average_water = float(lines[7].split(':')[1].strip())
+        self.cloud_fraction = float(lines[8].split(':')[1].strip())
+        self.cos_sza = float(lines[9].split(':')[1].strip())
+
+    def parse_integrator_specific_parameters(self, lines):
+        self.kpp_h_val = float(lines[0].split(':')[1].strip())
+        self.internal_timesteps = int(float(lines[0-1].split(':')[1].strip()))
+    
+    def parse_chemical_state(self, lines):
+        # obin: Based on Emy's code for parsing all values
+        self.concentrations = []
+        self.rate_constants = []
+        self.reaction_rates = []
+        for line in lines:
+            if line.startswith(' C('):
+                parts = line.split('=')
+                value = float(parts[1].strip())
+                self.concentrations.append(value)
+            elif line.startswith(' R('):
+                parts = line.split('=')
+                value = float(parts[1].strip())
+                self.rate_constants.append(value)
+            elif line.startswith(' A('):
+                parts = line.split('=')
+                value = float(parts[1].strip())
+                self.reaction_rates.append(value)
+    
+# load in a data example: assumed local, though a full path can be specified
+# filename = "Amazon_L1_20180101_2100.txt"
+# amazon_surface = ChemicalCase(filename)
